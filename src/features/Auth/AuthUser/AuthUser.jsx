160bb9f4b897efd19282b02bdf3483091b966ed1
Joao Portes <joaoluisp35@gmail.com>
2025-05-01 22:23:11 -0300

    Melhoria na Estilização da Interface

--- src/features/Auth/AuthUser/AuthUser.jsx
+++ src/features/Auth/AuthUser/AuthUser.jsx
@@ -1,27 +1,35 @@
 import styles from "./AuthUser.module.css";
+import { useNavigate } from 'react-router-dom';
 import imagemPrato from "../../../assets/imagem-prato.png";
 import '../../../Styles/global.css';
 
 export default function AuthUser() {
-  
+  const navigate = useNavigate();
+  const handleVoltarParaTelaInicial = () => {
+    navigate('/');
+  };
+
   return (
     <div className={styles.cadastroContainer}>
-      <div className={`${styles.ladoEsquerdo} ${styles.bgSilhueta}`}>
-        <div className={styles.logoContainer}>
-          <img
-            src={imagemPrato}
-            alt="Prato branco"
-            className={styles.imagemPrato}
-          />
-          {/* Texto dentro do prato */}
+      <div>
+        <img
+          src={imagemPrato}
+          alt="Prato branco"
+          className={styles.imagemPrato}
+        />
+        <div className={styles.sobreposicao}>
           <p className={styles.textConfAuth}>
             Sua autenticação foi concluída.
           </p>
           <p className={styles.textInfo}>
-            Aguarde o administrador <br/>validar seu cadastro.
+            Aguarde o administrador <br />validar seu cadastro.
           </p>
+          <button className={styles.btnCadastrar} onClick={handleVoltarParaTelaInicial}>
+          <i class="bi bi-arrow-left"></i> Voltar Para Tela Inicial
+          </button>
         </div>
       </div>
     </div>
+
   );
 }
